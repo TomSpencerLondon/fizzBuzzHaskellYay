8d05b4a507ef918adadfed4ecd4ea00e22294a77
Tom <thomas.spencer@codurance.com>
2020-08-27 09:54:39 +0100

    Add tests for 3, 5 and 15

--- test/FizzBuzzSpec.hs
+++ test/FizzBuzzSpec.hs
@@ -10,3 +10,6 @@ spec :: Spec
 spec = do
   describe "fizzBuzz" $ do
     it "returns 1 for 1" $ fizzval 1 `shouldBe` "1"
+    it "returns Fizz for 3" $ fizzval 3 `shouldBe` "Fizz"
+    it "returns Buzz for 5" $ fizzval 5 `shouldBe` "Buzz"
+    it "returns Fizzbuzz for 15" $ fizzval 15 `shouldBe` "FizzBuzz"
